b8eaadc831f4b656370e9889e281938c647a73b6
Roman Belash <n9necage@gmail.com>
2022-03-31 18:48:21 +0400

    Lecturers_rating

--- main.py
+++ main.py
@@ -4,22 +4,27 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
-        super().__init__(self, name, surname)
+        super().__init__(name, surname)
+        self.grades = {}
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
-        super().__init__(self, name, surname)
+        super().__init__(name, surname)
+
+    def rate_hw(self, student, course, grade):
+        if 0 <= grade <= 10:
+            if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+                if course in student.grades:
+                    student.grades[course] += [grade]
+                else:
+                    student.grades[course] = [grade]
+            else:
+                return 'Ошибка'
+        else:
+            return 'Оценивается от 0 до 10'
 
 class Student:
     def __init__(self, name, surname, gender):
@@ -30,13 +35,25 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def lecturer_rating(self, lecturer, course, grade):
+        if 0 <= grade <= 10:
+            if isinstance(lecturer, Lecturer) and course.name in self.courses_in_progress and course in lecturer.courses_attached:
+                if course in lecturer.grades:
+                    lecturer.grades[course] += [grade]
+                else:
+                    lecturer.grades[course] = [grade]
+            else:
+                return 'Ошибка'
+        else:
+            return 'Оценивается от 0 до 10'
+
 
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
 cool_mentor.rate_hw(best_student, 'Python', 10)
