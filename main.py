c332f8a70d530f5b3f52ef3a0cfaf69daec5feaf
Roman Belash <n9necage@gmail.com>
2022-04-01 16:27:45 +0400

    Final version

--- main.py
+++ main.py
@@ -9,9 +9,16 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
-        self.average_score = 0
+        self.avarage_score = 0
 
-    def total_average_score(self):
+    def add_course_to_attached(self, course):
+        if course.name not in self.courses_attached:
+            self.courses_attached.append(course.name)
+            course.lecturers[self.name] = []
+        else:
+            return 'Error'
+
+    def total_avarage_score(self):
         total = []
         count = 0
         for i in list(self.grades.values()):
@@ -19,55 +26,57 @@ class Lecturer(Mentor):
                 total.append(k)
         for i in total:
             count += i
-        self.average_score = round((count / len(total)), 2)
+        self.avarage_score = round((count / len(total)), 2)
+
 
     def __verify_data(cls, other):
         if not isinstance(other, (float, Lecturer)):
-            raise TypeError('float or Lecturer')
-        return other if isinstance(other, float) else other.average_score
+            raise TypeError
 
-    def __le__(self, other):
-        average_score = self.__verify_data(other)
-        return self.average_score <= average_score
-
-    def __lt__(self, other):
-        average_score = self.__verify_data(other)
-        return self.average_score < average_score
+        return other if isinstance(other, float) else other.avarage_score
 
     def __eq__(self, other):
-        average_score = self.__verify_data(other)
-        return self.average_score == average_score
+        avarage_score = self.__verify_data(other)
+        return self.avarage_score == avarage_score
+
+    def __lt__(self, other):
+        avarage_score = self.__verify_data(other)
+        return self.avarage_score < avarage_score
+
+    def __le__(self, other):
+        avarage_score = self.__verify_data(other)
+        return self.avarage_score <= avarage_score
 
     def __str__(self):
-        return f'''Имя: {self.name}
-Фамилия: {self.surname}
-Средняя оценка за лекции: {self.average_score}
+        return f'''Name: {self.name}
+Surname: {self.surname}
+Avarage score: {self.avarage_score}
 '''
 
 
-
 class Reviewer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
 
     def rate_hw(self, student, course, grade):
         if 0 <= grade <= 10:
-            if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-                if course in student.grades:
-                    student.grades[course] += [grade]
+            if isinstance(student, Student) and course.name in self.courses_attached and course.name in student.courses_in_progress:
+                if course.name in student.grades:
+                    student.grades[course.name] += [grade]
+                    course.students[student.name] += [grade]
                 else:
-                    student.grades[course] = [grade]
+                    student.grades[course.name] = [grade]
+                    course.students[student.name] = [grade]
             else:
                 return 'Error'
         else:
             return 'From 0 to 10'
-
-        student.total_average_score()
+        student.total_avarage_score()
+        course.students_avarage_score()
 
     def __str__(self):
-        return f'''Имя: {self.name}
-Фамилия: {self.surname}
+        return f'''Name: {self.name}
+Surname: {self.surname}
 '''
+
 
 class Student:
     def __init__(self, name, surname, gender):
@@ -77,9 +86,16 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        self.average_score = 0
+        self.avarage_score = 0
 
-    def total_average_score(self):
+    def add_courses_in_progress(self, course):
+        if course.name not in self.courses_in_progress:
+            self.courses_in_progress.append(course.name)
+            course.students[self.name] = []
+        else:
+            return 'Error'
+
+    def total_avarage_score(self):
         total = []
         count = 0
         for i in list(self.grades.values()):
@@ -87,104 +103,139 @@ class Student:
                 total.append(k)
         for i in total:
             count += i
-        self.average_score = round((count / len(total)), 2)
-
+        self.avarage_score = round((count / len(total)), 2)
 
     def lecturer_rating(self, lecturer, course, grade):
         if 0 <= grade <= 10:
-            if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
-                if course in lecturer.grades:
-                    lecturer.grades[course] += [grade]
+            if isinstance(lecturer,
+                          Lecturer) and course.name in self.courses_in_progress and course.name in lecturer.courses_attached:
+                if course.name in lecturer.grades:
+                    lecturer.grades[course.name] += [grade]
+                    course.lecturers[lecturer.name] += [grade]
                 else:
-                    lecturer.grades[course] = [grade]
+                    lecturer.grades[course.name] = [grade]
+                    course.lecturers[lecturer.name] = [grade]
             else:
                 return 'Error'
         else:
             return 'From 0 to 10'
+        lecturer.total_avarage_score()
+        course.lecturers_avarage_score()
 
-        lecturer.total_average_score()
 
     def __verify_data(cls, other):
         if not isinstance(other, (float, Student)):
-            raise TypeError('float or Student')
-        return other if isinstance(other, float) else other.average_score
+            raise TypeError
 
-    def __le__(self, other):
-        average_score = self.__verify_data(other)
-        return self.average_score <= average_score
-
-    def __lt__(self, other):
-        average_score = self.__verify_data(other)
-        return self.average_score < average_score
+        return other if isinstance(other, float) else other.avarage_score
 
     def __eq__(self, other):
-        average_score = self.__verify_data(other)
-        return self.average_score == average_score
+        avarage_score = self.__verify_data(other)
+        return self.avarage_score == avarage_score
+
+    def __lt__(self, other):
+        avarage_score = self.__verify_data(other)
+        return self.avarage_score < avarage_score
+
+    def __le__(self, other):
+        avarage_score = self.__verify_data(other)
+        return self.avarage_score <= avarage_score
 
     def __str__(self):
-        courses_in_progress_list = ''
+        list_courses_in_progress = ''
         for i in self.courses_in_progress:
-            courses_in_progress_list += f'{i}, '
+            list_courses_in_progress += f'{i}, '
 
-        finished_courses_list = ''
+        list_finished_courses = ''
         for i in self.finished_courses:
-            finished_courses_list += f'{i}, '
+            list_finished_courses += f'{i}, '
 
-        return f'''Имя: {self.name}
-Фамилия: {self.surname}
-Средняя оценка за домашнее задание: {self.average_score}
-Курсы в процессе изучения: {courses_in_progress_list}
-Завершенные курсы: {finished_courses_list}
+        return f'''Name: {self.name}
+Surname: {self.surname}
+Homework avarage score: {self.avarage_score}
+Courses in progress: {list_courses_in_progress}
+Completed courses: {list_finished_courses}
 '''
 
 
+class Course:
+    def __init__(self, name):
+        self.name = name
+        self.students = {}
+        self.lecturers = {}
+        self.st_avarage_score = 0
+        self.lec_avarage_score = 0
+
+    def students_avarage_score(self):
+        total = []
+        count = 0
+        for i in list(self.students.values()):
+            for k in i:
+                total.append(k)
+        for i in total:
+            count += i
+        self.st_avarage_score = round((count / len(total)), 2)
+
+    def lecturers_avarage_score(self):
+        total = []
+        count = 0
+        for i in list(self.lecturers.values()):
+            for k in i:
+                total.append(k)
+        for i in total:
+            count += i
+        self.lec_avarage_score = round((count / len(total)), 2)
+
+    def __str__(self):
+        return f'''Course name: {self.name}
+Average score of students: {self.st_avarage_score}
+Average score of lecturers: {self.lec_avarage_score}
+'''
 
 
-
-
-
+python = Course('Python')
+git = Course('Git')
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python-developer']
-best_student.finished_courses += ['GIT']
+best_student.add_courses_in_progress(python)
+best_student.finished_courses += ['Git']
 
 fedor = Student('Fedor', 'Dostoyevskii', 'male')
-fedor.courses_in_progress += ['Python-developer']
-fedor.finished_courses += ['GIT']
+fedor.add_courses_in_progress(python)
+fedor.finished_courses += ['Git']
 
 cool_mentor = Reviewer('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python-developer']
+cool_mentor.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 7)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, python, 9)
+cool_mentor.rate_hw(best_student, python, 10)
+cool_mentor.rate_hw(best_student, python, 10)
 
-cool_mentor.rate_hw(best_student, 'Python', 8)
-cool_mentor.rate_hw(best_student, 'Python', 9)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(fedor, python, 10)
+cool_mentor.rate_hw(fedor, python, 8)
+cool_mentor.rate_hw(fedor, python, 10)
 
-kirill = Lecturer('Kirill', 'Nemolyaev')
-kirill.courses_attached += ['Python-developer']
-marina = Lecturer('Marina', 'Tsvetaeva')
-marina.courses_attached += ['Python-developer']
+frodo = Lecturer('Frodo', 'Baggins')
+frodo.add_course_to_attached(python)
+gendalf = Lecturer('Gendalf', 'Grey')
+gendalf.add_course_to_attached(python)
 
-best_student.lecturer_rating(kirill, 'Python', 10)
-best_student.lecturer_rating(kirill, 'Python', 9)
-best_student.lecturer_rating(marina, 'Python', 9)
-best_student.lecturer_rating(marina, 'Python', 7)
+best_student.lecturer_rating(frodo, python, 10)
+best_student.lecturer_rating(frodo, python, 8)
+best_student.lecturer_rating(gendalf, python, 8)
+best_student.lecturer_rating(gendalf, python, 6)
 
-fedor.lecturer_rating(kirill, 'Python', 9)
-fedor.lecturer_rating(kirill, 'Python', 9)
-fedor.lecturer_rating(marina, 'Python', 6)
-fedor.lecturer_rating(marina, 'Python', 7)
-
+fedor.lecturer_rating(frodo, python, 9)
+fedor.lecturer_rating(frodo, python, 9)
+fedor.lecturer_rating(gendalf, python, 6)
+fedor.lecturer_rating(gendalf, python, 7)
 
 print(best_student.grades)
 print(best_student)
-print(kirill)
+print(frodo)
 print(cool_mentor)
-print(best_student)
 print(best_student == fedor)
 print(best_student < fedor)
-print(kirill != marina)
-print(kirill > marina)
+print(frodo != gendalf)
+print(frodo > gendalf)
+print(python)
